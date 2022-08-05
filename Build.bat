4883abceb362f746d29e3606019db4ed8f8c9057
deadline-cxn <deadline@cityxen.net>
2022-08-05 04:43:33 -0400

    updates

--- Build.bat
+++ Build.bat
@@ -3,6 +3,6 @@ echo Build Script: Building %1
 call genkickass-script.bat -t C64 -o prg_files -m false -s false -l "RETRO_DEV_LIB"
 call KickAss.bat mlb.asm
 copy prg_files\\_in.spr x:\\www\\html\\m64\\in\\ML.X.SPR
-rename prg_files\\ mlb.all.prg prg_files\\mlb.prg
+mv prg_files\\mlb.all.prg prg_files\\mlb.prg
 copy prg_files\\mlb.prg x:\\www\\html\\m64\\MLB.PRG
 copy prg_files\\mlb.prg E:\\dev\\github\\cityxen\\meatloaf-specialty\\data\\BUILD_CBM\\mlb.prg
